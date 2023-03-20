e9da07aee192ef005a42646a708485815eac93ed
Ilie Lupu <ilie.lupu@thinslices.com>
2023-03-20 10:21:20 +0200

    refer a friend wrap text

--- lib/widgets/refer_a_friend.dart
+++ lib/widgets/refer_a_friend.dart
@@ -1,5 +1,6 @@
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 class ReferAFriend extends StatelessWidget {
   const ReferAFriend({super.key});
@@ -27,37 +28,37 @@ class ReferAFriend extends StatelessWidget {
           ),
           Container(
             width: MediaQuery.of(context).size.width - 48,
+            padding: const EdgeInsets.all(16.0),
             decoration: const BoxDecoration(
               borderRadius: BorderRadius.all(Radius.circular(10)),
               color: Colors.black,
             ),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 16.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Text(
-                          textAlign: TextAlign.center,
-                          "Invite a friend and get \$15",
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w700,
-                            color: Colors.white,
-                          ),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Flexible(
+                      child: Text(
+                        textAlign: TextAlign.center,
+                        "Invite a friend and get \$15",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white,
                         ),
-                      ],
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 25.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Text(
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 15.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Flexible(
+                        child: Text(
+                          softWrap: true,
                           textAlign: TextAlign.center,
                           "Give a friend promo code on \$15 and you'll get \$15.",
                           style: TextStyle(
@@ -66,75 +67,62 @@ class ReferAFriend extends StatelessWidget {
                             color: Colors.white,
                           ),
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                  DottedBorder(
-                    borderType: BorderType.RRect,
-                    radius: const Radius.circular(10),
-                    color: Colors.white,
-                    strokeWidth: 1.5,
-                    strokeCap: StrokeCap.round,
-                    dashPattern: const [5, 5],
-                    child: ClipRRect(
-                      borderRadius: const BorderRadius.all(Radius.circular(10)),
-                      child: Container(
-                        width: MediaQuery.of(context).size.width - 48,
-                        decoration: const BoxDecoration(
-                          color: Colors.black,
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                            vertical: 16.0,
-                            horizontal: 106.5,
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: const [
-                              Icon(
+                ),
+                DottedBorder(
+                  borderType: BorderType.RRect,
+                  radius: const Radius.circular(10),
+                  color: Colors.white,
+                  strokeWidth: 1.5,
+                  strokeCap: StrokeCap.round,
+                  dashPattern: const [5, 5],
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.all(Radius.circular(10)),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          PlatformIconButton(
+                              onPressed: () {
+                                print("copy refer a friend code");
+                              },
+                              padding: EdgeInsets.zero,
+                              icon: const Icon(
                                 Icons.copy_all,
                                 color: Colors.white,
-                              ),
-                              Text(
-                                "SLRS3124",
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w700,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ],
+                              )),
+                          const Text(
+                            "SLRS3124",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.white,
+                            ),
                           ),
+                        ]),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: PlatformElevatedButton(
+                      color: Colors.white,
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      onPressed: () {},
+                      child: const Text(
+                        'Invite friends',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
                         ),
                       ),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 13.0),
-                    child: Container(
-                      width: MediaQuery.of(context).size.width - 48,
-                      decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        color: Colors.white,
-                      ),
-                      child: const Padding(
-                        padding: EdgeInsets.symmetric(
-                          vertical: 16.0,
-                          horizontal: 106.5,
-                        ),
-                        child: Text(
-                          "Invite Friends",
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w700,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ],
