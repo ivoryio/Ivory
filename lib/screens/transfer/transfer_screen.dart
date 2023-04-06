29e7cfca7fc0d0f5a9c873ee86db2dd53e6457cb
szaszgyozo <szaszgyozo@gmail.com>
2023-04-06 17:26:50 +0300

    transfer screen WIP

--- lib/screens/transfer/transfer_screen.dart
+++ lib/screens/transfer/transfer_screen.dart
@@ -5,37 +5,112 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:solarisdemo/cubits/auth_cubit/auth_cubit.dart';
 
 import '../../models/person_model.dart';
+import '../../widgets/platform_text_input.dart';
 import '../../widgets/screen.dart';
 import '../../cubits/transfer/transfer_cubit.dart';
 
 class TransferScreen extends StatelessWidget {
-  const TransferScreen({super.key});
+  const TransferScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    Person? person = context.read<AuthCubit>().state.user?.person;
-    inspect(person);
+    final person = context.read<AuthCubit>().state.user?.person;
+
     return BlocProvider.value(
       value: TransferCubit(),
       child: BlocBuilder<TransferCubit, TransferState>(
         builder: (context, state) {
-          log('TransferScreen: $state');
-
           return Screen(
             title: "Transfer money",
             hideBottomNavbar: true,
-            child: Column(
-              children: [
-                const Text("Send from"),
-                Container(
-                    child: Column(
-                  children: const [
-                    Text("Main account"),
-                    Text("Name"),
-                    Text("IBAN"),
-                  ],
-                ))
-              ],
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Send from",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  // Replace empty Row widget with a child widget
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      border: Border.all(
+                        color: true //TO DO
+                            ? Colors.black
+                            : const Color(0xFFEAECF0),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 25.0,
+                        horizontal: 16,
+                      ),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: const [
+                              Text("asd"),
+                            ],
+                          ),
+                          Row(
+                            children: const [
+                              Text("asd"),
+                            ],
+                          ),
+                          Row(
+                            children: const [
+                              Text("asd"),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 32),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Payee information",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      PlatformTextInput(
+                        textLabel: "Name of the person/business",
+                        hintText: "e.g Solaris",
+                        keyboardType: TextInputType.text,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your phone number';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 16),
+                      PlatformTextInput(
+                        textLabel: "IBAN",
+                        hintText: "e.g DE84 1101 0101 4735 3658 36",
+                        keyboardType: TextInputType.text,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your phone number';
+                          }
+                          return null;
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           );
         },
