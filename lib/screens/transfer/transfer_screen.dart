5f322191f7df5d7c1113a19d2f4242f81e5f2665
szaszgyozo <szaszgyozo@gmail.com>
2023-04-12 11:31:20 +0300

    added congrats page

--- lib/screens/transfer/transfer_screen.dart
+++ lib/screens/transfer/transfer_screen.dart
@@ -164,8 +164,9 @@ class TransferScreen extends StatelessWidget {
                   padding: defaultScreenPadding,
                   child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: const [
-                        Text('TransactionStateConfirmed'),
+                        TransferSuccessful(),
                       ]),
                 ),
               );
@@ -421,13 +422,14 @@ class AmountInformationState extends State<AmountInformation> {
         const Text('Enter Amount:'),
         const SizedBox(height: 8),
         PlatformCurrencyInput(
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter your phone number';
-              }
-              return null;
-            },
-            controller: _amountController),
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return 'Please enter your phone number';
+            }
+            return null;
+          },
+          controller: _amountController,
+        ),
       ],
     );
   }
@@ -528,6 +530,57 @@ class TransferDetails extends StatelessWidget {
           ],
         ),
       ],
+    );
+  }
+}
+
+class TransferSuccessful extends StatelessWidget {
+  const TransferSuccessful({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: 133,
+            height: 133,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              color: Color(0xFF747474),
+            ),
+            child: const Center(
+              child: Icon(
+                Icons.image_outlined,
+                size: 65,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 35,
+          ),
+          const Text(
+            "Congratulations",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          const Text(
+            "The payments has been sent. You can review the payment in the Transaction section.",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 16,
+              color: Color(0XFF667085),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
