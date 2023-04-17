e5b4b4d7bb0768fb5c3057bf8fc55f146d67fa2c
Dan Diac <danut.diac@thinslices.com>
2023-04-17 11:10:54 +0300

    Move config values to environment variables

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,16 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'router/router.dart';
 import 'services/auth_service.dart';
 import 'cubits/auth_cubit/auth_cubit.dart';
 import 'themes/default_theme.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load();
+
   runApp(const MyApp());
 }
 
