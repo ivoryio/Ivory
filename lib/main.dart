e697f6f630ae1076c46727d286509f44b2b78fee
ionutgabrielalexa <aligaby2000@yahoo.com>
2023-05-24 17:05:24 +0300

    Implemented landscape view lock

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -11,8 +12,10 @@ import 'themes/default_theme.dart';
 
 Future<void> main() async {
   await dotenv.load();
-
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+          [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
+      .then((_) => runApp(const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
