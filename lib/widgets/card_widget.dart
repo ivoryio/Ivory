3deb01fb7807771e4f1de8441fb9772c7dd9001a
ionutgabrielalexa <aligaby2000@yahoo.com>
2023-07-14 09:35:35 +0300

    Updated the background of the card

--- lib/widgets/card_widget.dart
+++ lib/widgets/card_widget.dart
@@ -39,117 +39,135 @@ class BankCardWidget extends StatelessWidget {
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(6),
         ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            vertical: defaultCardVerticalPadding,
+        child: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.bottomRight,
+              end: Alignment.topLeft,
+              stops: [0.0, 0.5629],
+              colors: [
+                Color(0xFF585858),
+                Color(0xFF000000),
+              ],
+              transform: GradientRotation(135 * (3.1415926 / 180.0)),
+            ),
+            image: DecorationImage(
+              image: AssetImage('assets/images/porsche_logo.png'),
+              fit: BoxFit.scaleDown,
+            ),
           ),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(right: 0),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.only(
-                        left: defaultCardHorizontalPadding,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: defaultCardVerticalPadding,
+            ),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 0),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(
+                          left: defaultCardHorizontalPadding,
+                        ),
+                        child: VisaSvgIcon(),
                       ),
-                      child: VisaSvgIcon(),
-                    ),
-                    if (isPrimary) const PrimaryBankCardLabel(),
-                    if (isViewable!) const EyeIcon(),
-                    if (cardType != null) CardTypeLabel(cardType: cardType!),
-                  ],
+                      if (isPrimary) const PrimaryBankCardLabel(),
+                      if (isViewable!) const EyeIcon(),
+                      if (cardType != null) CardTypeLabel(cardType: cardType!),
+                    ],
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(
-                  defaultCardHorizontalPadding,
-                  15,
-                  defaultCardHorizontalPadding,
-                  25,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    ...cardNumberParts.map((cardNumberPart) {
-                      Text textContent = Text(
-                        cardNumberPart,
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w600,
-                          fontSize: 24,
-                          letterSpacing: 3,
-                        ),
-                      );
-                      if (cardNumberPart == "****") {
-                        return SizedBox(height: 20, child: textContent);
-                      }
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(
+                    defaultCardHorizontalPadding,
+                    15,
+                    defaultCardHorizontalPadding,
+                    25,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ...cardNumberParts.map((cardNumberPart) {
+                        Text textContent = Text(
+                          cardNumberPart,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 24,
+                            letterSpacing: 3,
+                          ),
+                        );
+                        if (cardNumberPart == "****") {
+                          return SizedBox(height: 20, child: textContent);
+                        }
 
-                      return textContent;
-                    })
-                  ],
+                        return textContent;
+                      })
+                    ],
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SpacedColumn(
-                      space: 3,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text(
-                          "CARD HOLDER",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 12,
-                            height: 15 / 12,
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SpacedColumn(
+                        space: 3,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "CARD HOLDER",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                              height: 15 / 12,
+                            ),
                           ),
-                        ),
-                        Text(
-                          cardHolder,
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                            height: 20 / 16,
-                            fontWeight: FontWeight.w600,
+                          Text(
+                            cardHolder,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              height: 20 / 16,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          )
+                        ],
+                      ),
+                      SpacedColumn(
+                        space: 3,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          const Text(
+                            "EXPIRES",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                              height: 15 / 12,
+                            ),
                           ),
-                        )
-                      ],
-                    ),
-                    SpacedColumn(
-                      space: 3,
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        const Text(
-                          "EXPIRES",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 12,
-                            height: 15 / 12,
+                          Text(
+                            cardExpiry,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              height: 20 / 16,
+                              fontWeight: FontWeight.w600,
+                            ),
                           ),
-                        ),
-                        Text(
-                          cardExpiry,
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                            height: 20 / 16,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              )
-            ],
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
