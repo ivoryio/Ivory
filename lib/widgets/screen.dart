da4cbd42093e2ab47d459e11c962b565e195e053
ionutgabrielalexa <aligaby2000@yahoo.com>
2023-07-20 11:35:53 +0300

    Remove Platform widget and Cupertino items

--- lib/widgets/screen.dart
+++ lib/widgets/screen.dart
@@ -42,7 +42,7 @@ class Screen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    PlatformAppBar? appBar = hideAppBar == true
+    AppBar? appBar = hideAppBar == true
         ? null
         : createAppBar(
             context,
@@ -58,19 +58,13 @@ class Screen extends StatelessWidget {
 
     int currentPageIndex = AppRouter.calculateSelectedIndex(context);
 
-    PlatformTabController tabController = PlatformTabController(
-      initialIndex: currentPageIndex,
-    );
-
     ScrollPhysics? physics = onRefresh != null
         ? scrollPhysics ?? const AlwaysScrollableScrollPhysics()
         : null;
 
     if (hideBottomNavbar) {
-      return PlatformScaffold(
+      return Scaffold(
         appBar: appBar,
-        iosContentPadding: true,
-        iosContentBottomPadding: true,
         body: LayoutBuilder(builder:
             (BuildContext context, BoxConstraints viewportConstraints) {
           num bottomStickyWidgetHeight = bottomStickyWidget?.height ?? 0;
@@ -112,45 +106,9 @@ class Screen extends StatelessWidget {
       );
     }
 
-    return PlatformTabScaffold(
-      iosContentPadding: true,
-      tabController: tabController,
-      iosContentBottomPadding: true,
-      appBarBuilder: (context, index) => appBar,
-      itemChanged: (pageIndex) {
-        if (currentPageIndex != pageIndex) {
-          AppRouter.navigateToPage(pageIndex, context);
-        }
-      },
-      materialTabs: (context, platform) => MaterialNavBarData(
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Colors.white,
-        elevation: 0,
-      ),
-      cupertinoTabs: (context, platform) => CupertinoTabBarData(
-        backgroundColor: Colors.white,
-        activeColor: Colors.black,
-        inactiveColor: Colors.grey,
-      ),
-      items: const [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: 'Home',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.add_card),
-          label: 'Wallet',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.payments),
-          label: 'Transactions',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.person),
-          label: 'Profile',
-        ),
-      ],
-      bodyBuilder: (context, index) => LayoutBuilder(
+    return Scaffold(
+      appBar: appBar,
+      body: LayoutBuilder(
         builder: (BuildContext context, BoxConstraints viewportConstraints) {
           Widget screenContent = SingleChildScrollView(
             physics: physics,
@@ -174,6 +132,35 @@ class Screen extends StatelessWidget {
           });
         },
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentPageIndex,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        onTap: (index) {
+          if (currentPageIndex != index) {
+            AppRouter.navigateToPage(index, context);
+          }
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_card),
+            label: 'Wallet',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.payments),
+            label: 'Transactions',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
     );
   }
 }
@@ -184,10 +171,8 @@ class LoadingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PlatformScaffold(
-      iosContentPadding: true,
+    return Scaffold(
       appBar: createAppBar(context, title: title!),
-      iosContentBottomPadding: true,
       body: Center(
         child: PlatformCircularProgressIndicator(),
       ),
@@ -205,16 +190,14 @@ class ErrorScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PlatformScaffold(
-      iosContentPadding: true,
+    return Scaffold(
       appBar: createAppBar(context, title: title!),
-      iosContentBottomPadding: true,
       body: Text(message!),
     );
   }
 }
 
-PlatformAppBar createAppBar(
+AppBar createAppBar(
   BuildContext context, {
   bool? hideBackButton,
   Icon? backButtonIcon,
@@ -248,9 +231,6 @@ PlatformAppBar createAppBar(
       alignment: Alignment.centerLeft,
       padding: const EdgeInsets.only(left: defaultScreenHorizontalPadding),
     ),
-    cupertino: (context, platform) => CupertinoIconButtonData(
-      alignment: Alignment.centerLeft,
-    ),
     onPressed: () {
       if (customBackButtonCallback != null) {
         customBackButtonCallback();
@@ -262,24 +242,18 @@ PlatformAppBar createAppBar(
     },
   );
 
-  return PlatformAppBar(
+  return AppBar(
     leading: hideBackButton == true ? null : backButton,
     title: centerTitle == true ? titleText : leftAlignedTitle,
     backgroundColor: backgroundColor,
-    trailingActions: trailingActions,
-    material: (context, platform) =>
-        MaterialAppBarData(elevation: 0, centerTitle: centerTitle, actions: [
+    elevation: 0,
+    centerTitle: centerTitle,
+    actions: [
       if (trailingActions != null) ...trailingActions,
       const SizedBox(
         width: defaultScreenHorizontalPadding,
       )
-    ]),
-    cupertino: (context, platform) => CupertinoNavigationBarData(
-      border: Border.all(color: Colors.transparent),
-      padding: const EdgeInsetsDirectional.symmetric(
-        horizontal: defaultScreenHorizontalPadding,
-      ),
-    ),
+    ],
     automaticallyImplyLeading: hideBackButton == true ? false : true,
   );
 }
