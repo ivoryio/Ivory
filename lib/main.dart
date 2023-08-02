5397f17fb247db5f65338e18b5ea0901ecf9a2b2
Ilie Lupu <ilie.lupu@thinslices.com>
2023-08-02 14:17:28 +0300

    Added Ivory app initializer

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:solarisdemo/ivory_app.dart';
 
-import 'config.dart';
-import 'router/router.dart';
-import 'services/auth_service.dart';
-import 'cubits/auth_cubit/auth_cubit.dart';
+import '../config.dart';
 
 Future<void> main() async {
   await dotenv.load();
@@ -17,43 +14,5 @@ Future<void> main() async {
   await SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
 
-  runApp(MyApp(
-    clientConfig: clientConfig,
-  ));
-}
-
-class MyApp extends StatelessWidget {
-  final ClientConfigData clientConfig;
-
-  const MyApp({
-    super.key,
-    required this.clientConfig,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => AuthCubit(
-        authService: AuthService(),
-      ),
-      child: Builder(builder: (context) {
-        return MaterialApp.router(
-          routerConfig: AppRouter(context.read<AuthCubit>()).router,
-          localizationsDelegates: const [
-            DefaultMaterialLocalizations.delegate,
-            DefaultWidgetsLocalizations.delegate,
-            DefaultCupertinoLocalizations.delegate,
-          ],
-          theme: clientConfig.uiSettings.themeData,
-          builder: (context, child) {
-            return Scaffold(
-              body: Material(
-                child: child,
-              ),
-            );
-          },
-        );
-      }),
-    );
-  }
+  runApp(IvoryApp(clientConfig: clientConfig));
 }
