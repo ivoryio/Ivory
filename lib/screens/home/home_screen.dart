471e380580944e5e780c8185a8310eeb99077ed8
sergiu <sergiu.brinza@thinslices.com>
2023-08-03 17:38:53 +0300

    Change the UI of the home page main widget

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -3,23 +3,23 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
 
 import '../../config.dart';
+import '../../cubits/account_summary_cubit/account_summary_cubit.dart';
+import '../../cubits/auth_cubit/auth_cubit.dart';
+import '../../cubits/transaction_list_cubit/transaction_list_cubit.dart';
 import '../../models/user.dart';
-import '../../widgets/modal.dart';
-import '../../widgets/screen.dart';
-import '../../utilities/format.dart';
-import '../../widgets/analytics.dart';
-import 'modals/new_transfer_popup.dart';
-import '../../widgets/refer_a_friend.dart';
-import '../../services/person_service.dart';
-import '../../widgets/transaction_list.dart';
 import '../../router/routing_constants.dart';
+import '../../services/person_service.dart';
 import '../../services/transaction_service.dart';
 import '../../widgets/account_balance_text.dart';
-import '../../cubits/auth_cubit/auth_cubit.dart';
-import '../../cubits/account_summary_cubit/account_summary_cubit.dart';
-import '../../cubits/transaction_list_cubit/transaction_list_cubit.dart';
+import '../../widgets/analytics.dart';
+import '../../widgets/modal.dart';
+import '../../widgets/refer_a_friend.dart';
+import '../../widgets/screen.dart';
+import '../../widgets/transaction_list.dart';
+import 'modals/new_transfer_popup.dart';
 
 const _defaultCountTransactionsDisplayed = 3;
 const _defaultPage = 1;
@@ -52,14 +52,14 @@ class HomeScreen extends StatelessWidget {
           filter: _defaultTransactionListFilter,
         );
       },
-      title: 'Hello, ${user.cognito.firstName}!',
+      title: 'Welcome ${user.cognito.firstName}!',
       hideBackButton: true,
-      appBarColor: const Color(0xFF1C1A28),
+      appBarColor: Colors.black,
       trailingActions: [
         IconButton(
           padding: EdgeInsets.zero,
           icon: const Icon(
-            Icons.bar_chart,
+            Icons.remove_red_eye_outlined,
             color: Colors.white,
           ),
           onPressed: () {},
@@ -162,7 +162,7 @@ class HomePageHeader extends StatelessWidget {
                   bottomLeft: Radius.circular(30),
                   bottomRight: Radius.circular(30),
                 ),
-                color: Color(0xFF1C1A28),
+                color: Color(0xFF000000),
               ),
               child: const Center(
                 child: Padding(
@@ -186,16 +186,19 @@ class HomePageHeader extends StatelessWidget {
                   bottomLeft: Radius.circular(30),
                   bottomRight: Radius.circular(30),
                 ),
-                color: Color(0xFF1C1A28),
+                color: Colors.black,
               ),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   AccountSummary(
                     iban: state.data?.iban ?? "",
-                    income: state.data?.income ?? 0,
                     spending: state.data?.spending ?? 0,
                     availableBalance: state.data?.availableBalance?.value ?? 0,
+                  ),
+                  const Divider(
+                    color: Colors.white,
+                    thickness: 0.5,
                   ),
                   const AccountOptions(),
                 ],
@@ -213,36 +216,27 @@ class HomePageHeader extends StatelessWidget {
 class AccountSummary extends StatelessWidget {
   final String iban;
   final num availableBalance;
-  final num income;
   final num spending;
 
   const AccountSummary({
     super.key,
     required this.iban,
     required this.availableBalance,
-    required this.income,
     required this.spending,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(8)),
-        color: Color(0xFF272735),
-      ),
-      child: Column(
-        children: [
-          AccountBalance(
-            iban: iban,
-            value: availableBalance,
-          ),
-          AccountStats(
-            income: income,
-            spending: spending,
-          ),
-        ],
-      ),
+    return Column(
+      children: [
+        AccountBalance(
+          iban: iban,
+          value: availableBalance,
+        ),
+        AccountStats(
+          spending: spending,
+        ),
+      ],
     );
   }
 }
@@ -259,90 +253,110 @@ class AccountBalance extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(8),
-          topRight: Radius.circular(8),
-        ),
-        color: Colors.black,
-      ),
-      width: MediaQuery.of(context).size.width,
-      padding: const EdgeInsets.symmetric(vertical: 8),
-      child: Column(
-        children: [
-          const Text(
-            "Total Balance",
-            style: TextStyle(color: Colors.white),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                AccountBalanceText(
-                  value: value,
-                  numberStyle: const TextStyle(color: Colors.white),
-                  centsStyle: const TextStyle(color: Colors.white),
-                ),
-              ],
+    AuthenticatedUser user = context.read<AuthCubit>().state.user!;
+
+    return Column(
+      children: [
+        const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Available Balance",
+              style: TextStyle(color: Colors.white),
             ),
+            SizedBox(
+              width: 4,
+            ),
+            Icon(
+              Icons.info_outline,
+              color: Colors.white,
+            ),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              AccountBalanceText(
+                value: value,
+                numberStyle: const TextStyle(color: Colors.white, fontSize: 40),
+                centsStyle: const TextStyle(color: Colors.white, fontSize: 24),
+              ),
+            ],
           ),
-          Text(
-            "IBAN: ${Format.iban(iban)}",
-            style: const TextStyle(color: Colors.white),
-          )
-        ],
-      ),
+        ),
+        LinearPercentIndicator(
+          lineHeight: 8,
+          barRadius: const Radius.circular(40),
+          percent: ((user.personAccount.spending?.value ?? 0) /
+                      (user.personAccount.accountLimit?.value ?? 0))
+                  .isInfinite
+              ? 0
+              : (user.personAccount.spending?.value ?? 0) /
+                  (user.personAccount.accountLimit?.value ?? 0),
+          backgroundColor: const Color(0xFF313038),
+          progressColor: const Color(0xFFCC0000),
+          curve: Curves.fastOutSlowIn,
+        ),
+      ],
     );
   }
 }
 
 class AccountStats extends StatelessWidget {
-  final num income;
   final num spending;
 
   const AccountStats({
     super.key,
-    required this.income,
     required this.spending,
   });
 
   @override
   Widget build(BuildContext context) {
+    AuthenticatedUser user = context.read<AuthCubit>().state.user!;
+
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+      padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 16),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Row(
-            children: [
-              const Text("Income",
-                  style: TextStyle(
-                    color: Colors.white,
-                  )),
-              const SizedBox(width: 5),
-              Text(Format.euro(income),
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  )),
-            ],
-          ),
-          Row(
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const Text(
-                "Spending",
-                style: TextStyle(color: Colors.white),
+                "Outstanding balance",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                ),
               ),
               const SizedBox(width: 5),
-              Text(
-                Format.euro(spending),
-                style: const TextStyle(
+              AccountBalanceText(
+                value: spending,
+                numberStyle: const TextStyle(color: Colors.white, fontSize: 24),
+                centsStyle: const TextStyle(color: Colors.white, fontSize: 14),
+              ),
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              const Text(
+                "Credit limit",
+                style: TextStyle(
                   color: Colors.white,
-                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
                 ),
+              ),
+              const SizedBox(width: 5),
+              AccountBalanceText(
+                value: user.personAccount.accountLimit?.value ?? 0,
+                numberStyle: const TextStyle(color: Colors.white, fontSize: 24),
+                centsStyle: const TextStyle(color: Colors.white, fontSize: 14),
               ),
             ],
           ),
@@ -358,31 +372,29 @@ class AccountOptions extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 20),
+      padding: const EdgeInsets.symmetric(
+        vertical: 20,
+        horizontal: 24,
+      ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           AccountOptionsButton(
-            textLabel: "Top up",
-            icon: Icons.add_card,
-            onPressed: () => log("Top up"),
+            textLabel: "Transfer",
+            icon: Icons.compare_arrows,
+            onPressed: () => log("Transfer"),
           ),
           AccountOptionsButton(
-            textLabel: "Send",
-            icon: Icons.compare_arrows,
+            textLabel: "Repayments",
+            icon: Icons.currency_exchange,
             onPressed: () => showBottomModal(
               context: context,
               child: const NewTransferPopup(),
             ),
           ),
           AccountOptionsButton(
-            textLabel: "Request",
-            icon: Icons.receipt_long,
-            onPressed: () => log("Request"),
-          ),
-          AccountOptionsButton(
             textLabel: "Account",
-            icon: Icons.info,
+            icon: Icons.info_outline,
             onPressed: () {
               context.push(accountDetailsRoute.path);
             },
@@ -411,12 +423,12 @@ class AccountOptionsButton extends StatelessWidget {
         ElevatedButton(
           onPressed: () => onPressed(),
           style: ElevatedButton.styleFrom(
-            backgroundColor: const Color(0xFF272735),
+            backgroundColor: const Color(0xFFFFFFFF),
             fixedSize: const Size(50, 50),
             shape: const CircleBorder(),
             splashFactory: NoSplash.splashFactory,
           ),
-          child: Icon(icon, color: Colors.white),
+          child: Icon(icon, color: Colors.black),
         ),
         Padding(
           padding: const EdgeInsets.only(top: 10),
