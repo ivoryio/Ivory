6f307cd13969a49ef4b4ad06fdf9a9222daf8204
Tudor <tudor.iordache@thinslices.com>
2023-08-04 13:26:14 +0300

    Delete filtering bloc state files

--- lib/screens/transactions/transactions_filtering_screen.dart
+++ lib/screens/transactions/transactions_filtering_screen.dart
@@ -18,7 +18,6 @@ import '../../widgets/pill_button.dart';
 import '../../router/routing_constants.dart';
 import '../../widgets/spaced_column.dart';
 import 'modals/transaction_date_picker_popup.dart';
-import '../../cubits/transactions_filtering/transactions_filtering_cubit.dart';
 
 class TransactionsFilteringScreen extends StatefulWidget {
   final TransactionListFilter? transactionListFilter;
@@ -33,125 +32,127 @@ class TransactionsFilteringScreen extends StatefulWidget {
 }
 
 class _TransactionsFilteringScreenState extends State<TransactionsFilteringScreen> {
+
+  TransactionListFilter? transactionListFilter;
+  @override
+  void initState() {
+    transactionListFilter = widget.transactionListFilter;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    TransactionListFilter? transactionListFilter = widget.transactionListFilter;
+    bool isFilterSelected =
+        transactionListFilter?.bookingDateMin != null ||
+            transactionListFilter?.bookingDateMax != null;
 
-    return BlocProvider.value(
-      value: TransactionsFilteringCubit(
-        transactionListFilter:
-            transactionListFilter ?? const TransactionListFilter(),
+    AuthenticatedUser user = context
+        .read<AuthCubit>()
+        .state
+        .user!;
+    return Screen(
+      title: transactionsFilteringRoute.title,
+      hideBottomNavbar: true,
+      backButtonIcon: const Icon(
+        Icons.close,
+        color: Colors.black,
       ),
-      child:
-          BlocBuilder<TransactionsFilteringCubit, TransactionsFilteringState>(
-        builder: (context, state) {
-          final isFilterSelected =
-              transactionListFilter?.bookingDateMin != null ||
-                  transactionListFilter?.bookingDateMax != null;
-
-          AuthenticatedUser user = context.read<AuthCubit>().state.user!;
-          return Screen(
-            title: transactionsFilteringRoute.title,
-            hideBottomNavbar: true,
-            backButtonIcon: const Icon(
-              Icons.close,
-              color: Colors.black,
+      trailingActions: [
+        PlatformTextButton(
+          padding: EdgeInsets.zero,
+          onPressed: () {
+            context.push(transactionsRoute.path);
+          },
+          child: const Text(
+            "Reset filters",
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: 16,
             ),
-            trailingActions: [
-              PlatformTextButton(
-                padding: EdgeInsets.zero,
-                onPressed: () {
-                  context.read<TransactionsFilteringCubit>().resetFilters();
-                  context.push(transactionsRoute.path);
-                },
-                child: const Text(
-                  "Reset filters",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 16,
-                  ),
-                ),
-              ),
-            ],
-            child: Padding(
-              padding:
-                  ClientConfig.getCustomClientUiSettings().defaultScreenPadding,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SpacedColumn(
-                    space: 16.5,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text("By date"),
-                      Row(
-                        children: [
-                          const Icon(
-                            Icons.today,
-                            size: 15,
-                          ),
-                          const SizedBox(
-                            width: 8,
-                          ),
-                          PillButton(
-                            active:transactionListFilter?.bookingDateMin !=
-                                    null ||
-                                transactionListFilter?.bookingDateMax !=
-                                    null,
-                            buttonText:
-                                '${getFormattedDate(date: transactionListFilter?.bookingDateMin, text: "Start date")} - ${getFormattedDate(date: transactionListFilter?.bookingDateMax, text: "End date")}',
-                            buttonCallback: () {
-                              showBottomModal(
-                                context: context,
-                                child: TransactionDatePickerPopup(
-                                  initialSelectedRange: isFilterSelected
-                                      ? DateTimeRange(
-                                          start: transactionListFilter!.bookingDateMin!,
-                                          end:transactionListFilter!.bookingDateMax!)
-                                      : null,
-                                  onDateRangeSelected: (DateTimeRange range) {
-                                    transactionListFilter = TransactionListFilter(
-                                      bookingDateMin: range.start,
-                                      bookingDateMax: range.end,
-                                    );
-                                    print(transactionListFilter);
-                                  },
-                                ),
-                              );
-                            },
-                            icon: (transactionListFilter?.bookingDateMin !=
-                                        null ||
-                                    transactionListFilter?.bookingDateMax !=
-                                        null)
-                                ? const Icon(
-                                    Icons.close,
-                                    size: 16,
-                                  )
+          ),
+        ),
+      ],
+      child: Padding(
+        padding:
+        ClientConfig
+            .getCustomClientUiSettings()
+            .defaultScreenPadding,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SpacedColumn(
+              space: 16.5,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text("By date"),
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.today,
+                      size: 15,
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    PillButton(
+                      active: transactionListFilter?.bookingDateMin !=
+                          null ||
+                          transactionListFilter?.bookingDateMax !=
+                              null,
+                      buttonText:
+                      '${getFormattedDate(
+                          date: transactionListFilter?.bookingDateMin, text: "Start date")} - ${getFormattedDate(
+                          date: transactionListFilter?.bookingDateMax, text: "End date")}',
+                      buttonCallback: () {
+                        showBottomModal(
+                          context: context,
+                          child: TransactionDatePickerPopup(
+                            initialSelectedRange: isFilterSelected
+                                ? DateTimeRange(
+                                start: transactionListFilter!.bookingDateMin!,
+                                end: transactionListFilter!.bookingDateMax!)
                                 : null,
+                            onDateRangeSelected: (DateTimeRange range) {
+                              setState(() {
+                                transactionListFilter = TransactionListFilter(
+                                  bookingDateMin: range.start,
+                                  bookingDateMax: range.end,
+                                );
+                              });
+                            },
                           ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: PrimaryButton(
-                            text: "Apply filters",
-                            onPressed: () {
-                              print(transactionListFilter);
-                              StoreProvider.of<AppState>(context).dispatch(GetTransactionsCommandAction(filter: transactionListFilter, user: user.cognito));
-                              context.pop();
-                            }),
-                      ),
-                    ],
-                  )
-                ],
-              ),
+                        );
+                      },
+                      icon: (transactionListFilter?.bookingDateMin !=
+                          null ||
+                          transactionListFilter?.bookingDateMax !=
+                              null)
+                          ? const Icon(
+                        Icons.close,
+                        size: 16,
+                      )
+                          : null,
+                    ),
+                  ],
+                ),
+              ],
             ),
-          );
-        },
+            Row(
+              children: [
+                Expanded(
+                  child: PrimaryButton(
+                      text: "Apply filters",
+                      onPressed: () {
+                        StoreProvider.of<AppState>(context).dispatch(
+                            GetTransactionsCommandAction(filter: transactionListFilter, user: user.cognito));
+                        context.pop();
+                      }),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
