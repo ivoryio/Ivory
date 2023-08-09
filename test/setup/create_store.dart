c8405fae73cbdb1a9ccd1d4889731a36d4e05d7d
alexhagiu-ts <alex.hagiu@thinslices.com>
2023-08-09 15:23:25 +0300

    -fixed createTestStore

--- test/setup/create_store.dart
+++ test/setup/create_store.dart
@@ -4,20 +4,35 @@ import 'package:solarisdemo/models/transaction_model.dart';
 import 'package:solarisdemo/models/user.dart';
 import 'package:solarisdemo/redux/app_state.dart';
 import 'package:solarisdemo/redux/store_factory.dart';
+import 'package:solarisdemo/services/push_notification_service.dart';
 
 Store<AppState> createTestStore({
   required AppState initialState,
   TransactionService? transactionService,
+  PushNotificationService? pushNotificationService,
 }) {
   return createStore(
-      initialState: initialState,
-      transactionService: transactionService ?? NotImplementedTransactionService(),
+    initialState: initialState,
+    transactionService: transactionService ?? NotImplementedTransactionService(),
+    pushNotificationService: pushNotificationService ?? NotImplementedPushNotificationService(),
   );
 }
 
 class NotImplementedTransactionService extends TransactionService {
   @override
   Future<TransactionsServiceResponse> getTransactions({TransactionListFilter? filter, User? user}) {
-    throw(UnimplementedError);
+    throw UnimplementedError();
+  }
+}
+
+class NotImplementedPushNotificationService extends PushNotificationService {
+  @override
+  void init(Store<AppState> store, {User? user}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> hasPermission() {
+    throw UnimplementedError();
   }
 }
