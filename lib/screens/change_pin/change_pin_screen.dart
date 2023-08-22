244afb10af382d512c24c6183efc70ffd0191361
sergiu.brinza <sergiubrnz@gmail.com>
2023-08-22 11:39:56 +0300

    Add pin validations

--- lib/screens/change_pin/change_pin_screen.dart
+++ lib/screens/change_pin/change_pin_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
+import 'package:solarisdemo/cubits/auth_cubit/auth_cubit.dart';
+import 'package:solarisdemo/models/user.dart';
 import 'package:solarisdemo/widgets/app_toolbar.dart';
 import 'package:solarisdemo/widgets/screen_scaffold.dart';
 
@@ -10,6 +14,11 @@ class ChangePinScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    ValueNotifier<bool> birthdayErrorNotifier = ValueNotifier<bool>(false);
+    ValueNotifier<bool> postalCodeErrorNotifier = ValueNotifier<bool>(false);
+    ValueNotifier<bool> sequenceErrorNotifier = ValueNotifier<bool>(false);
+    ValueNotifier<bool> repeatingErrorNotifier = ValueNotifier<bool>(false);
+
     return ScreenScaffold(
       body: Column(
         children: [
@@ -48,21 +57,123 @@ class ChangePinScreen extends StatelessWidget {
               backgroundColor: Color(0xFFADADB4),
             ),
           ),
-          ChangePinBody(),
+          ChangePinBody(
+            birthdayErrorNotifier: birthdayErrorNotifier,
+            postalCodeErrorNotifier: postalCodeErrorNotifier,
+            sequenceErrorNotifier: sequenceErrorNotifier,
+            repeatingErrorNotifier: repeatingErrorNotifier,
+          ),
           const Spacer(),
-          const ChangePinChecks(),
+          ChangePinChecks(
+            birthdayErrorNotifier: birthdayErrorNotifier,
+            postalCodeErrorNotifier: postalCodeErrorNotifier,
+            sequenceErrorNotifier: sequenceErrorNotifier,
+            repeatingErrorNotifier: repeatingErrorNotifier,
+          ),
         ],
       ),
     );
   }
 }
 
-class ChangePinBody extends StatelessWidget {
+class ChangePinBody extends StatefulWidget {
+  final ValueNotifier<bool> birthdayErrorNotifier;
+  final ValueNotifier<bool> postalCodeErrorNotifier;
+  final ValueNotifier<bool> sequenceErrorNotifier;
+  final ValueNotifier<bool> repeatingErrorNotifier;
+
+  const ChangePinBody({
+    Key? key,
+    required this.birthdayErrorNotifier,
+    required this.postalCodeErrorNotifier,
+    required this.sequenceErrorNotifier,
+    required this.repeatingErrorNotifier,
+  }) : super(key: key);
+
+  @override
+  State<ChangePinBody> createState() => _ChangePinBodyState();
+}
+
+class _ChangePinBodyState extends State<ChangePinBody> {
   final TextEditingController _controller = TextEditingController();
-  ChangePinBody({Key? key}) : super(key: key);
+  final FocusNode _focusPin = FocusNode();
+  String _newPIN = '';
+  bool hasError = false;
+
+  bool hasConsecutiveDigits(String pin) {
+    if (pin.length < 4) {
+      widget.sequenceErrorNotifier.value = false;
+      return true;
+    }
+    for (int i = 0; i < pin.length - 1; i++) {
+      int currentDigit = int.parse(pin[i]);
+      int nextDigit = int.parse(pin[i + 1]);
+
+      if (nextDigit != currentDigit + 1) {
+        widget.sequenceErrorNotifier.value = false;
+        return true;
+      }
+    }
+    widget.sequenceErrorNotifier.value = true;
+    return false;
+  }
+
+  bool hasRepeatingDigits(String pin) {
+    if (pin.length < 4) {
+      widget.repeatingErrorNotifier.value = false;
+      return true;
+    }
+
+    Map<String, int> digitCount = {};
+
+    for (int i = 0; i < pin.length; i++) {
+      String digit = pin[i];
+      digitCount[digit] = (digitCount[digit] ?? 0) + 1;
+    }
+
+    for (var value in digitCount.values) {
+      if (value > 2) {
+        widget.repeatingErrorNotifier.value = true;
+        return false;
+      }
+    }
+
+    widget.repeatingErrorNotifier.value = false;
+    return true;
+  }
+
+  bool containsPostalCode(String pin, String postalCode) {
+    if (pin.length < 4) {
+      widget.postalCodeErrorNotifier.value = false;
+      return true;
+    }
+    if (postalCode.contains(_newPIN)) {
+      widget.postalCodeErrorNotifier.value = true;
+      return false;
+    }
+    widget.postalCodeErrorNotifier.value = false;
+    return true;
+  }
+
+  bool containsBirthDate(String pin, DateTime birtDate) {
+    if (pin.length < 4) {
+      widget.birthdayErrorNotifier.value = false;
+      return true;
+    }
+    String formattedDate = DateFormat('yyyyMMdd').format(birtDate);
+
+    if (formattedDate.contains(_newPIN)) {
+      widget.birthdayErrorNotifier.value = true;
+      return false;
+    }
+    widget.birthdayErrorNotifier.value = false;
+    return true;
+  }
 
   @override
   Widget build(BuildContext context) {
+    AuthenticatedUser user = context.read<AuthCubit>().state.user!;
+
     return Padding(
       padding: const EdgeInsets.all(24.0),
       child: Column(
@@ -89,26 +200,36 @@ class ChangePinBody extends StatelessWidget {
           const SizedBox(
             height: 32,
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: List.generate(4, (index) {
-              return Container(
-                width: 10,
-                height: 10,
-                margin: const EdgeInsets.symmetric(horizontal: 12),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16),
-                  color: const Color(0xffadadb4),
-                ),
-              );
-            }),
+          GestureDetector(
+            onTap: () {
+              _focusPin.requestFocus();
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: List.generate(4, (index) {
+                return Container(
+                  width: 10,
+                  height: 10,
+                  margin: const EdgeInsets.symmetric(horizontal: 12),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    color: hasError
+                        ? const Color(0xffE61F27)
+                        : index >= _newPIN.length
+                            ? const Color(0xffadadb4)
+                            : const Color(0xff15141E),
+                  ),
+                );
+              }),
+            ),
           ),
-          Container(
+          SizedBox(
             width: MediaQuery.of(context).size.width,
             height: 1,
             child: TextField(
               autofocus: true,
               controller: _controller,
+              focusNode: _focusPin,
               inputFormatters: [
                 FilteringTextInputFormatter.digitsOnly,
               ],
@@ -123,7 +244,15 @@ class ChangePinBody extends StatelessWidget {
               cursorRadius: const Radius.circular(0),
               cursorWidth: 0,
               onChanged: (text) {
-                print("Typed text: $text");
+                if (text.length <= 4) {
+                  setState(() {
+                    _newPIN = text;
+                    hasError = !hasConsecutiveDigits(_newPIN) ||
+                        !containsPostalCode(_newPIN, user.person.address?.postalCode ?? 'postalCode') ||
+                        !hasRepeatingDigits(_newPIN) ||
+                        !containsBirthDate(_newPIN, user.person.birthDate ?? DateTime.now());
+                  });
+                }
               },
             ),
           ),
@@ -134,98 +263,133 @@ class ChangePinBody extends StatelessWidget {
 }
 
 class ChangePinChecks extends StatelessWidget {
-  const ChangePinChecks({Key? key}) : super(key: key);
+  final ValueNotifier<bool> birthdayErrorNotifier;
+  final ValueNotifier<bool> postalCodeErrorNotifier;
+  final ValueNotifier<bool> sequenceErrorNotifier;
+  final ValueNotifier<bool> repeatingErrorNotifier;
+
+  const ChangePinChecks({
+    Key? key,
+    required this.birthdayErrorNotifier,
+    required this.postalCodeErrorNotifier,
+    required this.sequenceErrorNotifier,
+    required this.repeatingErrorNotifier,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(24.0),
+    return Padding(
+      padding: const EdgeInsets.all(24.0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
+          const Text(
             "Your PIN should not contain:",
             style: TextStyle(
               fontSize: 16,
               fontWeight: FontWeight.w600,
             ),
           ),
-          SizedBox(
+          const SizedBox(
             height: 8,
           ),
-          Row(
-            children: [
-              Icon(
-                Icons.close,
-                size: 24,
-              ),
-              SizedBox(
-                width: 4,
-              ),
-              Text(
-                "Your date of birth",
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Icon(
-                Icons.close,
-                size: 24,
-              ),
-              SizedBox(
-                width: 4,
-              ),
-              Text(
-                "Your postal code",
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Icon(
-                Icons.close,
-                size: 24,
-              ),
-              SizedBox(
-                width: 4,
-              ),
-              Text(
-                "Number sequences, e.g. 1234",
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Icon(
-                Icons.close,
-                size: 24,
-              ),
-              SizedBox(
-                width: 4,
-              ),
-              Text(
-                "More than two digits repeating",
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-            ],
-          ),
+          ValueListenableBuilder<bool>(
+              valueListenable: birthdayErrorNotifier,
+              builder: (context, hasError, child) {
+                return Row(
+                  children: [
+                    Icon(
+                      Icons.close,
+                      size: 24,
+                      color: birthdayErrorNotifier.value ? const Color(0xffE61F27) : Colors.black,
+                    ),
+                    const SizedBox(
+                      width: 4,
+                    ),
+                    Text(
+                      "Your date of birth",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                        color: birthdayErrorNotifier.value ? const Color(0xffE61F27) : Colors.black,
+                      ),
+                    ),
+                  ],
+                );
+              }),
+          ValueListenableBuilder<bool>(
+              valueListenable: postalCodeErrorNotifier,
+              builder: (context, hasError, child) {
+                return Row(
+                  children: [
+                    Icon(
+                      Icons.close,
+                      size: 24,
+                      color: postalCodeErrorNotifier.value ? const Color(0xffE61F27) : Colors.black,
+                    ),
+                    const SizedBox(
+                      width: 4,
+                    ),
+                    Text(
+                      "Your postal code",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                        color: postalCodeErrorNotifier.value ? const Color(0xffE61F27) : Colors.black,
+                      ),
+                    ),
+                  ],
+                );
+              }),
+          ValueListenableBuilder<bool>(
+              valueListenable: sequenceErrorNotifier,
+              builder: (context, hasError, child) {
+                return Row(
+                  children: [
+                    Icon(
+                      Icons.close,
+                      size: 24,
+                      color: sequenceErrorNotifier.value ? const Color(0xffE61F27) : Colors.black,
+                    ),
+                    const SizedBox(
+                      width: 4,
+                    ),
+                    Text(
+                      "Number sequences, e.g. 1234",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                        color: sequenceErrorNotifier.value ? const Color(0xffE61F27) : Colors.black,
+                      ),
+                    ),
+                  ],
+                );
+              }),
+          ValueListenableBuilder<bool>(
+              valueListenable: repeatingErrorNotifier,
+              builder: (context, hasError, child) {
+                return Row(
+                  children: [
+                    Icon(
+                      Icons.close,
+                      size: 24,
+                      color: repeatingErrorNotifier.value ? const Color(0xffE61F27) : Colors.black,
+                    ),
+                    const SizedBox(
+                      width: 4,
+                    ),
+                    Text(
+                      "More than two digits repeating",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                        color: repeatingErrorNotifier.value ? const Color(0xffE61F27) : Colors.black,
+                      ),
+                    ),
+                  ],
+                );
+              }),
         ],
       ),
     );
