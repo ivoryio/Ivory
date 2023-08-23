fc2d82330892028c4b9050f91dac0e75c038ce56
Ilie Lupu <ilie.lupu@thinslices.com>
2023-08-23 12:51:37 +0300

    card rounded corners

---
+++ lib/widgets/card_rounded_corners.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class CardRoundedCorners extends StatelessWidget {
+  final BorderRadiusGeometry borderRadius;
+  final Color color;
+  final Widget child;
+  final EdgeInsets padding;
+
+  const CardRoundedCorners({
+    super.key,
+    required this.child,
+    this.color = const Color(0xFFF8F9FA),
+    this.borderRadius = const BorderRadius.all(Radius.circular(16)),
+    this.padding = EdgeInsets.zero,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: borderRadius,
+        color: color,
+      ),
+      padding: padding,
+      child: child,
+    );
+  }
+}
