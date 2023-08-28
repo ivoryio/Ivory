45b42ae25aec11aec8fe8dbe546093861ab04e17
Ilie Lupu <ilie.lupu@thinslices.com>
2023-08-28 16:27:09 +0300

    use textStyle from client config

--- lib/widgets/ivory_generic_error.dart
+++ lib/widgets/ivory_generic_error.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:solarisdemo/config.dart';
 
 class IvoryGenericError extends StatelessWidget {
   final String errorMessage;
-  final TextStyle textStyle;
+  final TextStyle? textStyle;
 
   const IvoryGenericError({
     super.key,
     this.errorMessage = 'Something went wrong',
-    this.textStyle = const TextStyle(
-      color: Colors.red,
-      fontSize: 16,
-      fontWeight: FontWeight.bold,
-    ),
+    this.textStyle,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Text(errorMessage, style: textStyle);
+    return Text(
+      errorMessage,
+      style: textStyle ?? ClientConfig.getTextStyleScheme().bodyLargeRegularBold.copyWith(color: Colors.red),
+    );
   }
 }
