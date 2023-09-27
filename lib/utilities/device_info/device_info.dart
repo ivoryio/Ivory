5dc7e65026c73d7b3fbb7f373087cfee8d2adac5
ionutgabrielalexa <aligaby2000@yahoo.com>
2023-09-27 14:54:51 +0300

    Add try-catch block in getDeviceName() function

--- lib/utilities/device_info/device_info.dart
+++ lib/utilities/device_info/device_info.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter/foundation.dart';
 
 class DeviceInfoService {
   final DeviceInfoPlugin _deviceInfoPlugin = DeviceInfoPlugin();
@@ -8,20 +9,25 @@ class DeviceInfoService {
   DeviceInfoService();
 
   Future<String> getDeviceName() async {
-    if (Platform.isAndroid) {
-      AndroidDeviceInfo androidInfo = await _deviceInfoPlugin.androidInfo;
-      String manufacturer = androidInfo.manufacturer;
-      String model = androidInfo.model;
-      String response = '$manufacturer $model';
-      if (response.length > 20) {
-        return response.substring(0, 20);
-      }
+    try {
+      if (Platform.isAndroid) {
+        AndroidDeviceInfo androidInfo = await _deviceInfoPlugin.androidInfo;
+        String manufacturer = androidInfo.manufacturer;
+        String model = androidInfo.model;
+        String response = '$manufacturer $model';
+        if (response.length > 20) {
+          return response.substring(0, 20);
+        }
 
-      return response.substring(0, response.length);
-    } else if (Platform.isIOS) {
-      IosDeviceInfo iosInfo = await _deviceInfoPlugin.iosInfo;
-      return iosInfo.name;
+        return response.substring(0, response.length);
+      } else if (Platform.isIOS) {
+        IosDeviceInfo iosInfo = await _deviceInfoPlugin.iosInfo;
+        return iosInfo.name;
+      }
+      return 'Unknown device';
+    } catch (e) {
+      debugPrint(e.toString());
+      return 'Unknown device';
     }
-    return 'Unknown';
   }
 }
