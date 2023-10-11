3c661b4ca3f6296da9986eefca6bee81b01b8831
ilie-lupu-ts <127083262+ilie-lupu-ts@users.noreply.github.com>
2023-10-11 15:13:50 +0300

    added onboarding start screen

--- lib/screens/onboarding/start/onboarding_start_screen.dart
+++ lib/screens/onboarding/start/onboarding_start_screen.dart
@@ -1,9 +1,12 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:solarisdemo/config.dart';
 import 'package:solarisdemo/widgets/app_toolbar.dart';
+import 'package:solarisdemo/widgets/button.dart';
 import 'package:solarisdemo/widgets/screen_scaffold.dart';
 import 'package:solarisdemo/widgets/screen_title.dart';
+import 'package:solarisdemo/widgets/scrollable_screen_container.dart';
 
 class OnboardingStartScreen extends StatelessWidget {
   static const routeName = "/onboardingStartScreen";
@@ -14,6 +17,7 @@ class OnboardingStartScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenScaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           AppToolbar(
             padding: ClientConfig.getCustomClientUiSettings().defaultScreenHorizontalPadding,
@@ -22,11 +26,44 @@ class OnboardingStartScreen extends StatelessWidget {
             ],
           ),
           Expanded(
-            child: SingleChildScrollView(
+            child: ScrollableScreenContainer(
               padding: ClientConfig.getCustomClientUiSettings().defaultScreenHorizontalPadding,
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  ScreenTitle("It’s quick and easy!"),
+                  const ScreenTitle("It's quick and easy!"),
+                  const SizedBox(height: 16),
+                  Text.rich(
+                    TextSpan(
+                      children: [
+                        const TextSpan(text: "Get your credit card instantly in just "),
+                        TextSpan(
+                          text: "25 minutes!",
+                          style: ClientConfig.getTextStyleScheme().bodyLargeRegularBold,
+                        ),
+                      ],
+                      style: ClientConfig.getTextStyleScheme().bodyLargeRegular,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Text.rich(
+                    TextSpan(
+                      children: [
+                        const TextSpan(text: "First we'll ask you "),
+                        TextSpan(
+                          text: "two quick questions ",
+                          style: ClientConfig.getTextStyleScheme().bodyLargeRegularBold,
+                        ),
+                        const TextSpan(text: "to ensure our services are tailored to your needs.")
+                      ],
+                      style: ClientConfig.getTextStyleScheme().bodyLargeRegular,
+                    ),
+                  ),
+                  const Spacer(),
+                  Center(child: SvgPicture.asset("assets/images/onboarding_start.svg")),
+                  const Spacer(),
+                  SizedBox(width: double.infinity, child: PrimaryButton(text: "Let's start", onPressed: () {})),
+                  const SizedBox(height: 16)
                 ],
               ),
             ),
