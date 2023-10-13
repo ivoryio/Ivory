7529b77c7cf12dd128f995235f1952de603b0e05
ilie-lupu-ts <127083262+ilie-lupu-ts@users.noreply.github.com>
2023-10-13 13:30:01 +0300

    remove external storage permission request + show download failure notification

--- lib/infrastructure/file_saver_service.dart
+++ lib/infrastructure/file_saver_service.dart
@@ -1,8 +1,8 @@
+import 'dart:developer';
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:solarisdemo/infrastructure/notifications/push_notification_service.dart';
 
@@ -15,32 +15,47 @@ class FileSaverService {
       final xFile = XFile.fromData(bytes, name: name, mimeType: mimeType);
       Share.shareXFiles([xFile]);
     } else {
-      // Ask for storage permission
-      final status = await Permission.manageExternalStorage.request();
-      if (!status.isGranted) {
-        return;
-      }
+      try {
+        await _writeLocalFile(name: name, extension: ext!, bytes: bytes);
 
-      // Save to downloads
-      Directory saveDir = Directory('/storage/emulated/0/Download');
-      if (!saveDir.existsSync()) {
-        saveDir = Directory('/storage/emulated/0/Downloads');
-      }
-      final file = File('${saveDir.path}/$name$ext');
-      await file.writeAsBytes(bytes);
-
-      // Show notification that file is downloaded
-      FlutterLocalNotificationsPlugin().show(
-        name.hashCode,
-        'Bill downloaded',
-        'File is in your Downloads folder',
-        const NotificationDetails(
-          android: AndroidNotificationDetails(
-            highImportanceChannelId,
-            highImportanceChannelId,
+        FlutterLocalNotificationsPlugin().show(
+          name.hashCode,
+          'Bill downloaded',
+          'File is in your Downloads folder',
+          const NotificationDetails(
+            android: AndroidNotificationDetails(
+              highImportanceChannelId,
+              highImportanceChannelId,
+            ),
           ),
-        ),
-      );
+        );
+      } catch (error) {
+        FlutterLocalNotificationsPlugin().show(
+          name.hashCode,
+          'Bill download failed',
+          'Please try again',
+          const NotificationDetails(
+            android: AndroidNotificationDetails(
+              highImportanceChannelId,
+              highImportanceChannelId,
+            ),
+          ),
+        );
+      }
     }
+  }
+}
+
+Future<void> _writeLocalFile({required String name, required String extension, required Uint8List bytes}) async {
+  Directory downloadDirectory = Directory('/storage/emulated/0/Download');
+
+  final path = downloadDirectory.path;
+  final fileObject = File('$path/$name.$extension');
+
+  final file = await fileObject.writeAsBytes(bytes);
+
+  if (!(await file.exists())) {
+    log("File not saved");
+    throw Exception('File not saved');
   }
 }
