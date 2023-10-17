3907c33c2e25a0e31818e8fb2f493b449c0f6c41
ilie-lupu-ts <127083262+ilie-lupu-ts@users.noreply.github.com>
2023-10-17 10:14:21 +0300

    support for isLoading

--- lib/widgets/continue_button_controller.dart
+++ lib/widgets/continue_button_controller.dart
@@ -1,25 +1,44 @@
 import 'package:flutter/foundation.dart';
 
-class ContinueButtonController extends ValueNotifier<bool> {
-  ContinueButtonController({bool isEnabled = false}) : super(isEnabled);
+class ContinueButtonState {
+  final bool isEnabled;
+  final bool isLoading;
 
-  get isEnabled => value;
+  ContinueButtonState({this.isEnabled = false, this.isLoading = false});
+
+  ContinueButtonState copyWith({bool? isEnabled, bool? isLoading}) {
+    return ContinueButtonState(
+      isEnabled: isEnabled ?? this.isEnabled,
+      isLoading: isLoading ?? this.isLoading,
+    );
+  }
+}
+
+class ContinueButtonController extends ValueNotifier<ContinueButtonState> {
+  ContinueButtonController({bool isEnabled = false, bool isLoading = false})
+      : super(ContinueButtonState(isEnabled: isEnabled, isLoading: isLoading));
+
+  ContinueButtonState get state => value;
+
+  bool get isEnabled => value.isEnabled;
+  bool get isLoading => value.isLoading;
 
   void setEnabled() {
-    if (value == true) {
-      return;
-    }
-
-    value = true;
+    value = state.copyWith(isEnabled: true, isLoading: false);
     notifyListeners();
   }
 
   void setDisabled() {
-    if (value == false) {
+    value = value.copyWith(isEnabled: false, isLoading: false);
+    notifyListeners();
+  }
+
+  void setLoading() {
+    if (state.isLoading == true) {
       return;
     }
 
-    value = false;
+    value = value.copyWith(isLoading: true);
     notifyListeners();
   }
 
