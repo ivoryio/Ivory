82a798279c22a7a821289ce5b3dfeca14c5d6287
ionutgabrielalexa <aligaby2000@yahoo.com>
2023-10-23 17:23:12 +0300

    Added isDisabled property

--- lib/widgets/checkbox.dart
+++ lib/widgets/checkbox.dart
@@ -5,9 +5,14 @@ import '../config.dart';
 class CheckboxWidget extends StatefulWidget {
   final bool isChecked;
   final Function(bool) onChanged;
+  final bool isDisabled;
 
-  const CheckboxWidget(
-      {super.key, required this.isChecked, required this.onChanged});
+  const CheckboxWidget({
+    super.key,
+    required this.isChecked,
+    required this.onChanged,
+    this.isDisabled = false,
+  });
 
   @override
   State<StatefulWidget> createState() => _CheckboxWidgetState();
@@ -34,23 +39,29 @@ class _CheckboxWidgetState extends State<CheckboxWidget> {
             horizontal: VisualDensity.minimumDensity,
           ),
           activeColor: ClientConfig.getColorScheme().secondary,
-          side: MaterialStateBorderSide.resolveWith(
-                  (states) {
-                    if(states.contains((MaterialState.selected))) {
-                      return BorderSide(width: 1.0, color: ClientConfig.getColorScheme().secondary);
-                    }
-                    return const BorderSide(width: 1.0, color: Color.fromRGBO(110, 117, 124, 1));
-                  }),
+          side: MaterialStateBorderSide.resolveWith((states) {
+            if (states.contains(MaterialState.disabled)) {
+              return BorderSide(width: 1, color: ClientConfig.getCustomColors().neutral500);
+            }
+
+            if (states.contains((MaterialState.selected))) {
+              return BorderSide(width: 1, color: ClientConfig.getColorScheme().secondary);
+            }
+
+            return BorderSide(width: 1, color: ClientConfig.getCustomColors().neutral500);
+          }),
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(2.0),
           ),
           value: _isChecked,
-          onChanged: (checked) {
-            setState(() {
-              _isChecked = checked!;
-              widget.onChanged(checked);
-            });
-          },
+          onChanged: widget.isDisabled
+              ? null
+              : (checked) {
+                  setState(() {
+                    _isChecked = checked!;
+                    widget.onChanged(checked);
+                  });
+                },
         ),
       ),
     );
