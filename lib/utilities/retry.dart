0046b54fd948231155570ed1967f2110de480351
ilie-lupu-ts <127083262+ilie-lupu-ts@users.noreply.github.com>
2023-11-28 09:57:15 +0200

    update retry

--- lib/utilities/retry.dart
+++ lib/utilities/retry.dart
@@ -1,23 +1,23 @@
 Future<T> retry<T>(
   Future<T> Function() fn, {
-  int retries = 3,
+  int maxAttempts = 3,
   bool Function(T)? retryIf,
   Duration delay = const Duration(milliseconds: 500),
 }) async {
   try {
     final result = await fn();
 
-    if (retryIf != null && retryIf(result) && retries > 0) {
+    if (retryIf != null && retryIf(result) && maxAttempts > 0) {
       await Future.delayed(delay);
-      return retry(fn, retries: retries - 1, retryIf: retryIf, delay: delay);
+      return retry(fn, maxAttempts: maxAttempts - 1, retryIf: retryIf, delay: delay);
     }
 
     return result;
   } catch (error) {
-    if (retries > 0) {
+    if (maxAttempts > 0) {
       await Future.delayed(delay);
 
-      return retry(fn, retries: retries - 1, retryIf: retryIf, delay: delay);
+      return retry(fn, maxAttempts: maxAttempts - 1, retryIf: retryIf, delay: delay);
     } else {
       rethrow;
     }
