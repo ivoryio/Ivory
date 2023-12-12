452e7b0e0b240cfc18bce4f7d16178964e2438e6
ilie-lupu-ts <127083262+ilie-lupu-ts@users.noreply.github.com>
2023-12-12 15:29:40 +0200

    redirect to error screen when any error occurs

--- lib/screens/onboarding/identity_verification/onboarding_review_updated_contracts_screen.dart
+++ lib/screens/onboarding/identity_verification/onboarding_review_updated_contracts_screen.dart
@@ -1,26 +1,20 @@
-import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:solarisdemo/config.dart';
 import 'package:solarisdemo/infrastructure/documents/documents_presenter.dart';
 import 'package:solarisdemo/infrastructure/documents/documents_service.dart';
 import 'package:solarisdemo/infrastructure/onboarding/identity_verification/onboarding_identity_verification_presenter.dart';
-import 'package:solarisdemo/models/onboarding/onboarding_identification_status.dart';
 import 'package:solarisdemo/redux/app_state.dart';
 import 'package:solarisdemo/redux/documents/documents_action.dart';
 import 'package:solarisdemo/redux/onboarding/identity_verification/onboarding_identity_verification_action.dart';
+import 'package:solarisdemo/screens/onboarding/identity_verification/onboarding_identity_verification_error_screen.dart';
 import 'package:solarisdemo/screens/onboarding/identity_verification/onboarding_sign_with_tan_screen.dart';
-import 'package:solarisdemo/utilities/ivory_color_mapper.dart';
 import 'package:solarisdemo/widgets/animated_linear_progress_indicator.dart';
 import 'package:solarisdemo/widgets/app_toolbar.dart';
 import 'package:solarisdemo/widgets/button.dart';
 import 'package:solarisdemo/widgets/circular_loading_indicator.dart';
 import 'package:solarisdemo/widgets/documents_list_view.dart';
-import 'package:solarisdemo/widgets/ivory_asset_with_badge.dart';
 import 'package:solarisdemo/widgets/screen_scaffold.dart';
-import 'package:solarisdemo/widgets/screen_title.dart';
-import 'package:solarisdemo/widgets/scrollable_screen_container.dart';
 
 class OnboardingReviewUpdatedContractsScreen extends StatelessWidget {
   static const routeName = "/onboardingReviewUpdatedContractsScreen";
@@ -37,30 +31,32 @@ class OnboardingReviewUpdatedContractsScreen extends StatelessWidget {
       onWillChange: (previousViewModel, newViewModel) {
         if (newViewModel.isAuthorized == true) {
           Navigator.pushNamedAndRemoveUntil(context, OnboardingSignWithTanScreen.routeName, (_) => false);
+        } else if (newViewModel.errorType != null) {
+          Navigator.pushNamedAndRemoveUntil(
+            context,
+            OnboardingIdentityVerificationErrorScreen.routeName,
+            (route) => false,
+          );
         }
       },
+      distinct: true,
       builder: (context, viewModel) {
-        final isAuthorizationStatusValid = viewModel.identificationStatus == null ||
-            viewModel.identificationStatus == OnboardingIdentificationStatus.authorizationRequired;
-
         return ScreenScaffold(
-          body: isAuthorizationStatusValid && viewModel.errorType == null
-              ? Column(
-                  children: [
-                    AppToolbar(
-                      richTextTitle: StepRichTextTitle(step: 4, totalSteps: 7),
-                      actions: const [AppbarLogo()],
-                      padding: ClientConfig.getCustomClientUiSettings().defaultScreenHorizontalPadding,
-                    ),
-                    AnimatedLinearProgressIndicator.step(current: 4, totalSteps: 7),
-                    const SizedBox(height: 16),
-                    viewModel.isLoading == true && viewModel.identificationStatus == null
-                        ? _buildLoadingContent()
-                        : _buildPageContent(context, viewModel),
-                    const SizedBox(height: 16),
-                  ],
-                )
-              : _buildErrorPage(),
+          body: Column(
+            children: [
+              AppToolbar(
+                richTextTitle: StepRichTextTitle(step: 4, totalSteps: 7),
+                actions: const [AppbarLogo()],
+                padding: ClientConfig.getCustomClientUiSettings().defaultScreenHorizontalPadding,
+              ),
+              AnimatedLinearProgressIndicator.step(current: 4, totalSteps: 7),
+              const SizedBox(height: 16),
+              viewModel.isLoading == true && viewModel.identificationStatus == null
+                  ? _buildLoadingContent()
+                  : _buildPageContent(context, viewModel),
+              const SizedBox(height: 16),
+            ],
+          ),
         );
       },
     );
@@ -90,65 +86,10 @@ class OnboardingReviewUpdatedContractsScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildErrorPage() {
-    return ScrollableScreenContainer(
-      padding: ClientConfig.getCustomClientUiSettings().defaultScreenPadding,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const AppToolbar(),
-          const ScreenTitle("Your identity verification has failed"),
-          const SizedBox(height: 16),
-          Text.rich(
-            TextSpan(
-              style: ClientConfig.getTextStyleScheme().bodyLargeRegular,
-              children: [
-                const TextSpan(text: 'We regret to inform you that your '),
-                TextSpan(
-                  text: 'identity verification ',
-                  style: ClientConfig.getTextStyleScheme().bodyLargeRegularBold,
-                ),
-                const TextSpan(text: 'has not been successful due to a '),
-                TextSpan(
-                  text: 'technical issue',
-                  style: ClientConfig.getTextStyleScheme().bodyLargeRegularBold,
-                ),
-                const TextSpan(text: '.\n\n'),
-                const TextSpan(text: 'Please get in touch with us by tapping on the '),
-                TextSpan(
-                  text: 'button below',
-                  style: ClientConfig.getTextStyleScheme().bodyLargeRegularBold,
-                ),
-                const TextSpan(text: '.'),
-              ],
-            ),
-          ),
-          Expanded(
-            child: IvoryAssetWithBadge(
-              childWidget: SvgPicture(
-                SvgAssetLoader(
-                  'assets/images/repayment_more_credit.svg',
-                  colorMapper: IvoryColorMapper(
-                    baseColor: ClientConfig.getColorScheme().secondary,
-                  ),
-                ),
-              ),
-              childPosition: BadgePosition.topEnd(top: -5, end: 78),
-              isSuccess: false,
-            ),
-          ),
-          PrimaryButton(
-            text: "Contact us",
-            onPressed: () {},
-          ),
-          const SizedBox(height: 16),
-        ],
-      ),
-    );
-  }
-
   Widget _buildPageContent(
-      BuildContext context, OnboardingIdentityVerificationViewModel identityVerificationViewModel) {
+    BuildContext context,
+    OnboardingIdentityVerificationViewModel identityVerificationViewModel,
+  ) {
     return Expanded(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
